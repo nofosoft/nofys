f284352ef39facdf9b904de361e48e72cc9eec80
nofosoft <jmldn.en@gmail.com>
2026-04-17 17:03:38 +0700

    berhasil menggunakan vcpkg untuk install fmt

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#include <fmt/core.h>
 
-int main(int, char**){
-    std::cout << "Hello, from nofys!\n";
+int main()
+{
+    fmt::print("Hello from {}\n", "vcpkg + CMake");
+    return 0;
 }
