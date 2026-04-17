6623b44c62a699635a6619ccbcdc99f501057dab
nofosoft <jmldn.en@gmail.com>
2026-04-17 16:24:23 +0700

    Reconfigure struktur base project

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 
-int main(int, char **)
-{
-
-    while (1)
-    {
-        std::cout << "Hello, from nofys!\n";
-    }
-
-    return 0;
+int main(int, char**){
+    std::cout << "Hello, from nofys!\n";
 }
